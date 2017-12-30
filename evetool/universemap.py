41b58861c9a75a3ed42d5305b9322d41768400a7
Korin <0mza987@gmail.com>
2017-12-30 19:59:27 +0800

    adapt new jump range updates

--- evetool/universemap.py
+++ evetool/universemap.py
@@ -27,40 +27,32 @@ def inOneJump(start,dest):
 	x2, y2, z2= systemInfo[dsystem][1], systemInfo[dsystem][2], systemInfo[dsystem][3]
 	# calculate the distance between two systems in light years
 	distance = round(math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2)),2)
-	# s1 for Titans, Supercarriers, Carriers, Dreadnoughts, Rorquals
-	# s2 for Black ops
-	# s3 for Jumpfreighters
-	if distance<=4.5:
-		s1=s2=s3='JDC4, JDC5'
-	elif distance>4.5 and distance<=5:
-		s1='JDC5'
-		s2=s3='JDC4, JDC5'
-	elif distance>5 and distance<=7:
-		s1='Negative'
-		s2=s3='JDC4, JDC5'
-	elif distance>7 and distance<=8:
-		s1='Negative'
-		s2='JDC5'
-		s3='JDC4, JDC5'
-	elif distance>8 and distance<=9:
-		s1=s2='Negative'
-		s3='JDC4, JDC5'
-	elif distance>9 and distance<=10:
-		s1=s2='Negative'
-		s3='JDC5'
-	elif distance>10:
-		s1=s2=s3='Negative'
 
+	res=[]
+	# res[0] for Carriers, Dreadnoughts, FAX
+	# res[1] for Titans, Supercarriers 
+	# res[2] for Jumpfreighters, Rorquals
+	# res[3] for Black ops
+	benchmarks = [[6.5,7],[5.5,6],[9,10],[7.2,8]]
+	for i in range(4):
+		if distance <= benchmarks[i][0]:
+			ss='JDC4, JDC5'
+		elif distance <=benchmarks[i][1]:
+			ss='JDC5'
+		else:
+			ss='Negative'
+		res.append(ss)
 	result = """__**{0} -> {1}({2}ly)**__:
 
-__Titans__: {3}
-__Supercarriers__: {3}
 __Carriers__: {3}
 __Dreadnoughts__: {3}
-__Rorquals__: {3}
 
-__Black Ops__: {4}
+__Titans__: {4}
+__Supercarriers__: {4}
 
 __Jumpfreighters__: {5}
-	""".format(ssystem,dsystem,distance,s1,s2,s3)
+__Rorquals__: {5}
+
+__Black Ops__: {6}
+	""".format(ssystem,dsystem,distance,res[0],res[1],res[2],res[3])
 	return result
